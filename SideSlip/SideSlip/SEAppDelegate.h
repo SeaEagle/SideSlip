08dde1b5d51550fa0e4b2cff8be3cce5a99fde83
SeaEagle <admin@Eagle.local>
2014-12-30 17:47:56 +0800

    First time, initial the project 首次提交，初始化项目

---
+++ SideSlip/SideSlip/SEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SEAppDelegate.h
+//  SideSlip
+//
+//  Created by SeaEagle on 14/12/30.
+//  Copyright (c) 2014年 Eagle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SideSlip/SideSlip/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SideSlip
+//
+//  Created by SeaEagle on 14/12/30.
+//  Copyright (c) 2014年 Eagle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SEAppDelegate class]));
+    }
+}
